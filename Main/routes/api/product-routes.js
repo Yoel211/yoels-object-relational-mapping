b1daf1a44fe6f22cb68ca1850027eb4d279887bd
Yoel Regalado <yoelregalado7@gmail.com>
2023-03-28 15:36:30 -0600

    updated api routes

--- Main/routes/api/product-routes.js
+++ Main/routes/api/product-routes.js
@@ -4,19 +4,39 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try {
+    const productData = await Product.findAll ({
+      include: [{model: Category, Tag }],
+    });
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{model: Category, Tag }],
+    });
+    if (!productData){
+      res.status(404).json({message: 'No Product data found with that ID!'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -48,7 +68,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
--- Main/routes/api/tag-routes.js
+++ Main/routes/api/tag-routes.js
@@ -62,29 +62,6 @@ router.put('/:id', async (req, res) => {
 });
 
 
-// // UPDATE a user
-// router.put('/:id', async (req, res) => {
-//   try {
-//     const userData = await User.update(req.body, {
-//       where: {
-//         id: req.params.id,
-//       },
-//     });
-//     if (!userData[0]) {
-//       res.status(404).json({ message: 'No user with this id!' });
-//       return;
-//     }
-//     res.status(200).json(userData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-
-
-
-
-
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
