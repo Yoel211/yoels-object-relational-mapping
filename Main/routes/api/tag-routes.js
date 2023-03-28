4f1ef7224af6781e119f3aa04118360f46491c59
Yoel Regalado <yoelregalado7@gmail.com>
2023-03-28 14:50:17 -0600

    updated tag route

--- Main/routes/api/tag-routes.js
+++ Main/routes/api/tag-routes.js
@@ -45,7 +45,45 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await tagData.update(req.body, {
+      where: {
+        id:req.params.id,
+      },
+    });
+    if (!userData[0]) {
+        res.status(404).json({ message: 'No Tag with this id!' });
+        return;
+        }
+        res.status(200).json(userData);
+    } catch (error) {
+    res.status(500).json(err);
+  }
 });
+
+
+// // UPDATE a user
+// router.put('/:id', async (req, res) => {
+//   try {
+//     const userData = await User.update(req.body, {
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
+//     if (!userData[0]) {
+//       res.status(404).json({ message: 'No user with this id!' });
+//       return;
+//     }
+//     res.status(200).json(userData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+
+
+
+
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
